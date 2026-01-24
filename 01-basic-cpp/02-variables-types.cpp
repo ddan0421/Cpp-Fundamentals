e7aea3f6c5c4188916eb86fd7c5be729102320e8
Dan Dan <ddanzk28@gmail.com>
2026-01-24 13:48:27 -0800

    02-variables-types: add additional data types

--- 01-basic-cpp/02-variables-types.cpp
+++ 01-basic-cpp/02-variables-types.cpp
@@ -14,6 +14,15 @@ int main(){
   std::cout << "Final amount is " << final_money << "you fine with this? \n";
   std::cout << "Sure Dan! \n";
 
+  // Determining the Size of a Data Type 
+  // The sizeof operator gives the size of any data type or variable:
+  std::cout << "A double is stored in "
+            << sizeof(double) << "bytes \n";
+
+  std::cout << "The variable final_money is stored in "
+            << sizeof(final_money) << "bytes \n";
+
+
   // char type
   char grade = 'C';
 
@@ -33,6 +42,30 @@ int main(){
   std::cout << "I am a female? Response: " << female_flag << std::endl;
   std::cout << "I am handsome? Response: " << handsome_flag << std::endl;
 
+
+  // auto key word
+  /* 
+   * The auto key word tells the compiler to determine the variable's
+   * Data type from the initialization value.
+   */  
+  auto amount = 100;
+  auto interestRate = 12.0;
+  auto stockCode = 'D';
+  auto customerNum = 459L; //Long
+
+  std::cout << "This is customer Num which is long type: " << customerNum << "\n";
+
+  // Basic operators other than + - * / 
+  std::cout << 13 % 5 << std::endl;
+  // the % calculates the remainder. It requires integers for both operands 
+
+  /*
+   * Named constant (constant variables) are those content cannot be 
+   * changed during program execution
+   */ 
+
+  const double TAX_RATE = 0.0675;
+  const int NUM_STATES = 50;
 
   return 0;
   
