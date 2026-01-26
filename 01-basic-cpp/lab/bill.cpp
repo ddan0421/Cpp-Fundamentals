eca45f3f1afc285ad2bf9becd1718c52be62b46b
Dan Dan <ddanzk28@gmail.com>
2026-01-25 21:32:42 -0800

    01-basic-cpp/lab/bill: add answers for bill

--- 01-basic-cpp/lab/bill.cpp
+++ 01-basic-cpp/lab/bill.cpp
@@ -3,7 +3,7 @@
 // The input will come from the keyboard and the output will go to
 // the screen.
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
 #include <iomanip>
@@ -19,15 +19,20 @@ int main()
 	cout << "Please input the number of items bought" << endl;
 
 	// Fill in the input statement to bring in the quantity.
+  cin >> quantity;
 
 	// Fill in the prompt to ask for the price.
+  cout << "Please input the price of each item" << endl;
 
 	// Fill in the input statement to bring in the price of each item.
+  cin >> itemPrice;
 
 	// Fill in the assignment statement to determine the total bill.
+  totalBill = quantity * itemPrice;
 
 	// Fill in the output statement to print total bill,
 	// with a label to the screen.
+  cout << "The total bill is $" << totalBill << endl;
 
 	return 0;
 }
