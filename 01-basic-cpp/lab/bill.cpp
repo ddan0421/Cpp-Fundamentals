5380640036e2a132554bd690c9eaf96e43c5a03d
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-01-26 18:15:52 -0800

    01-basic-cpp/lab/bill and tabledata: fix indentation

--- 01-basic-cpp/lab/bill.cpp
+++ 01-basic-cpp/lab/bill.cpp
@@ -12,33 +12,33 @@ using namespace std;
 
 int main()
 {
-	int	quantity;		// contains the amount of items purchased 
-	float itemPrice;	// contains the price of each item
-	float totalBill;	// contains the total bill.
+  int quantity;   // contains the amount of items purchased 
+  float itemPrice;  // contains the price of each item
+  float totalBill;  // contains the total bill.
   string itemName;
 
   cout << "Please input the name of the item" << endl;
   getline(cin, itemName);
 
-	cout << setprecision(2) << fixed << showpoint;	// formatted output 
-	cout << "Please input the number of items bought" << endl;
+  cout << setprecision(2) << fixed << showpoint;  // formatted output 
+  cout << "Please input the number of items bought" << endl;
 
-	// Fill in the input statement to bring in the quantity.
+  // Fill in the input statement to bring in the quantity.
   cin >> quantity;
 
-	// Fill in the prompt to ask for the price.
+  // Fill in the prompt to ask for the price.
   cout << "Please input the price of each item" << endl;
 
-	// Fill in the input statement to bring in the price of each item.
+  // Fill in the input statement to bring in the price of each item.
   cin >> itemPrice;
 
-	// Fill in the assignment statement to determine the total bill.
+  // Fill in the assignment statement to determine the total bill.
   totalBill = quantity * itemPrice;
 
-	// Fill in the output statement to print total bill,
-	// with a label to the screen.
+  // Fill in the output statement to print total bill,
+  // with a label to the screen.
   cout << "The item that you bought is " << itemName << endl;
   cout << "The total bill is $" << totalBill << endl;
 
-	return 0;
+  return 0;
 }
