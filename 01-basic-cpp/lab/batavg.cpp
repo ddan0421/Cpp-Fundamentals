67e22b4a1bbd16a3a99f6ab74aaf9ef90f3d0ece
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-01-26 18:49:48 -0800

    01-basic-cpp/lab/batavg: add LAB 3.4 Working with Type Casting exercise 3 content

--- 01-basic-cpp/lab/batavg.cpp
+++ 01-basic-cpp/lab/batavg.cpp
@@ -1,7 +1,7 @@
 // This program will determine the batting average of a player.
 // The number of hits and at bats are set internally in the program.
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
 using namespace std;
@@ -11,10 +11,10 @@ const int HITS = 123;
 
 int main()
 {
-	int batAvg;
+  float batAvg;
 
-	batAvg = HITS / AT_BAT									// an assignment statement 
-	cout << "The batting average is " << batAvg << endl;	// output the result
+  batAvg = static_cast<float> (HITS) / (AT_BAT);                  // an assignment statement 
+  cout << "The batting average is " << batAvg << endl;  // output the result
 
-	return 0;
+  return 0;
 }
