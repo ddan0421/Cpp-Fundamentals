ac05c5fc09df845ad3135ef98370eca491cedb66
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-01-26 17:59:03 -0800

    01-basic-cpp/lab/tabledata.cpp: modify the template by adding lab 3.2 formatting output exercise 1 content

--- 01-basic-cpp/lab/tabledata.cpp
+++ 01-basic-cpp/lab/tabledata.cpp
@@ -4,8 +4,8 @@
 //PLACE YOUR NAME HERE
 
 #include <iostream>
-#include	// Fill in the code to bring in the library for
-			// formatted output.
+#include <iomanip> // Fill in the code to bring in the library for
+                   // formatted output.
 using namespace std;
 
 int main()
@@ -18,19 +18,22 @@ int main()
 
 	// Fill in the input statement that reads in price1 and
 	// quantity1 from the keyboard.
+  cin >> price1 >> quantity1;
 
 	// Fill in the prompt for the second price and quantity.
-
+  cout << "Please input the price and quantity of the second item" << endl;
 	// Fill in the input statement that reads in price2 and
 	// quantity2 from the keyboard.
+  cin >> price2 >> quantity2;
 
 	cout << setw(15) << "PRICE" << setw(12) << "QUANTITY\n\n";
 
 	// Fill in the output statement that prints the first price
 	// and quantity. Be sure to use setw() statements.
-
+  cout << setw(15) << price1 << setw(12) << quantity1 << endl;
 	// Fill in the output statement that prints the second price
 	// and quantity.
+  cout << setw(15) << price2 << setw(12) << quantity2 << endl;
 
 	return 0;
 }
