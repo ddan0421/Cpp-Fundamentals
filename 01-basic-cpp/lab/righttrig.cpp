8e03653c373d6330df52c5aece7ce035c9238710
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-01-26 18:27:27 -0800

    01-basic-cpp/lab/righttrig: add lab 3.3 Arithmetic Operations and math functions exercise 2 content

--- 01-basic-cpp/lab/righttrig.cpp
+++ 01-basic-cpp/lab/righttrig.cpp
@@ -1,25 +1,28 @@
 // This program will input the value of two sides of a right triangle and then
 // determine the size of the hypotenuse.
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
-#include <cmath>	// needed for math functions like sqrt()
+#include <cmath>  // needed for math functions like sqrt()
+#include <iomanip>                
 using namespace std;
 
 int main()
 {
-	float a, b;	// the smaller two sides of the triangle 
-	float hyp;	// the hypotenuse calculated by the program
+  float a, b; // the smaller two sides of the triangle 
+  float hyp;  // the hypotenuse calculated by the program
 
-	cout << "Please input the value of the two sides" << endl;
-	cin >> a >> b;
+  cout << "Please input the value of the two sides" << endl;
+  cin >> a >> b;
 
-	// Fill in the assignment statement that determines the hypotenuse
+  // Fill in the assignment statement that determines the hypotenuse
+  hyp = sqrt(pow(a,2) + pow(b,2));
 
-	cout << "The sides of the right triangle are " << a << " and " << b << endl;
+  cout << "The sides of the right triangle are " << a << " and " << b << endl;
 
-	cout << "The hypotenuse is " << hyp << endl;
+  cout << setprecision(2) << fixed << showpoint;
+  cout << "The hypotenuse is " << hyp << endl;
 
-	return 0;
+  return 0;
 }
