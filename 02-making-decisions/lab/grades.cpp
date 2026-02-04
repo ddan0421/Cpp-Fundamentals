5adb9f522b23588b61ecc487b401731b0d95060b
Dan Dan <ddanzk28@gmail.com>
2026-02-03 22:04:02 -0800

    02-making-decisions/lab/grades.cpp: add content for grades.cpp in unit 2 lab

--- 02-making-decisions/lab/grades.cpp
+++ 02-making-decisions/lab/grades.cpp
@@ -13,11 +13,18 @@ int main()
   cout << "Input your average:" << endl;
   cin >> average;
 
-  if (average > 60)
+  if (average > 100)
+    cout << "Invalid data" << endl;
+  else if (average >= 90 && average <= 100)
+    cout << "'A' category" << endl;
+  else if (average >= 80 && average <= 89)
+    cout << "'B' category" << endl;
+  else if (average >= 60 && average <= 79) // average >= 60 will pass
     cout << "You Pass" << endl;
-
-  if (average < 60)
+  else if (average >= 0 && average <= 59)
     cout << "You Fail" << endl;
-
+  else
+    cout << "Invalid data" << endl; // extra line to handle average < 0
+                                
   return 0;
 }
