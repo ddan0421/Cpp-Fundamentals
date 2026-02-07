396372f010720477a65e2612df32229aa7413ded
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-02-06 21:28:52 -0800

    02-making-decisions/lab/switch.cpp: add content to switch.cpp and responses to switch_responses.txt

--- 02-making-decisions/lab/switch.cpp
+++ 02-making-decisions/lab/switch.cpp
@@ -12,20 +12,37 @@ int main()
   cout << "What grade did you earn in Programming I ?" << endl;
   cin >> grade;
 
-  switch (grade)  // This is where the switch statement begins
+  //Replace swithc statement with if else statement
+  if (grade == 'A')
   {
-    case 'A': cout << "an A - excellent work !" << endl;
-          break;
-    case 'B': cout << "you got a B - good job" << endl;
-              break;
-    case 'C': cout << "earning a C is satisfactory" << endl;
-              break;
-    case 'D': cout << "while D is passing, there is a problem" << endl;
-            break;
-    case 'F': cout << "you failed - better luck next time" << endl
-              break;
-    default: cout << "You did not enter an A, B, C, D, or F" << endl;
+    cout << "YOU PASSED!" << endl;
+    cout << "an A - excellent work !" << endl;
   }
+  else if (grade == 'B')
+  { 
+    cout << "YOU PASSED!" << endl;
+    cout << "you got a B - good job" << endl;
+  }
+  else if (grade == 'C')
+  {
+    cout << "YOU PASSED!" << endl;
+    cout << "earning a C is satisfactory" << endl;
+  }
+  else if (grade == 'D')
+  { 
+    cout << "YOU PASSED!" << endl;
+    cout << "while D is passing, there is a problem" << endl;
+  }
+  else if (grade == 'F')
+  {
+      cout << "you failed - better luck next time" << endl;
+  }
+  else 
+  {
+    cout << "You did not enter an A, B, C, D, or F" << endl;
+
+  }
+
 
   return 0;
 }
