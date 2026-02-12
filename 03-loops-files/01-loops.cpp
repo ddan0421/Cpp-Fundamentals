d609cc1827d0fa82339f81d3a5ded18cb656c025
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-02-11 18:33:25 -0800

    03-loops-files/01-loops: add first notes in unit 3

---
+++ 03-loops-files/01-loops.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+  // The increment and decrement operators
+  int a = 9;
+  a++;
+  
+  cout << a << endl;
+
+  int b = 10;
+
+  b--;
+
+  cout << b << endl;
+
+  // ++ and -- can be used before or after a variable;
+  
+  
+  // pre-and post-operations will cause different comparisons
+  
+  int age = 49;
+  if (age++ > 49)
+    cout << "Congratulations - You have made it to the half-century"
+         << " mark !" << endl;
+
+  // The cout statement will not execute. The reason is that in the postfix
+  // mode the comparison between age and 49 is made first. Then the value of age is 
+  // incremented by one.
+  //
+  // Things are much different if we replace the posfix operator with the prefix operator:
+  //
+  if (++age > 49)
+    cout << "Congratulations - You have made it to the half-century"
+         << " mark !" << endl;
+  //In this code age is incremented first. So its value is 50 when the comparison is
+  //made. The conditional statement is true and the cout statement is executed
+
+
+
+  return 0;
+
+}
+
