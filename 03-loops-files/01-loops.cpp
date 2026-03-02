1d9fa68d97f450c020b3ea3edfbf581a4fbb1ec9
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-03-01 18:46:13 -0800

    03-loops-files/01-loops.cpp: add loops lecture

--- 03-loops-files/01-loops.cpp
+++ 03-loops-files/01-loops.cpp
@@ -62,5 +62,109 @@ int main() {
 
   cout << " 5! = " << numFac << endl;
 
+  /*
+   *
+   * Counters - often a programmer needs to control the number of times a
+   * particular loop is repeated. One common way to accomplish this is by using
+   * a counter. A counter-controlled loop.
+   */
+
+  int score;
+  float total = 0.0;
+  float average;
+
+  const int NUMBEROFTESTS = 5;
+  int test = 1; // counter that controls the loop
+
+  while (test <= NUMBEROFTESTS) {
+    cout << "Enter your score on test " << test << ": " << endl;
+    cin >> score;
+
+    total = total + score;
+    test++;
+  }
+  average = total / NUMBEROFTESTS;
+
+  cout << "Your average based on " << NUMBEROFTESTS << " test scores is "
+       << average << endl;
+
+  /*
+   * Use while loop as input validation
+   * The loop is skipped for all valid input but for invalid input the loop is
+   * executed and prompts the user to enter new (valid) input.
+   */
+
+  cout << "Please input your choice of drink "
+       << "(a number from 1 to 4 or 0 to quit)" << endl;
+  cout << " 1 - Coffee" << endl
+       << " 2 - Tea" << endl
+       << " 3 - Coke" << endl
+       << " 4 - Orange Juice" << endl
+       << endl
+       << " 0 - QUIT" << endl
+       << endl;
+  int beverage;
+  cin >> beverage;
+  while (beverage < 0 || beverage > 4) {
+    cout << "Valid choices are 0 - 4. Please re-enter: ";
+    cin >> beverage;
+  }
+
+  /*
+   * do-while loop
+   * The while loop is a pre-test or top test loop.
+   * Since we test the expression before entering the loop,
+   * if the test expression in the while loop is initially false,
+   * then no iterations of the loop will be executed.
+   */
+  int num1 = 5;
+  int num2 = 7;
+  do {
+    num1 = num1 + 1;
+    num2 = num2 - 1;
+
+  } while (num2 < num1);
+  // In this code the statements num1 = num1 + 1 and num2 = num2 - 1 are
+  // executed exactly once. At this point num1 = 6 and num2 = 6 so the
+  // expression num2 < num1 is false. Consequently, the program exits the loop
+  // and moves to the next section of code.
+  // A do-while loops can be useful for programs that involve a repeating menu.
+
+  /* The for loop
+   * for (initialization; test; update)
+   *  statement; // or block in {}
+   * No semicolon after the update expression or after the )
+   */
+
+  int count;
+  for (count = 1; count <= 5; count++)
+    cout << "Hello" << endl;
+
+  int value;
+  int total_num = 0;
+  int number;
+  float mean;
+
+  cout << "Please enter a positive integer" << endl;
+  cin >> value;
+
+  if (value > 0) {
+    for (number = 1; number <= value; number++) {
+      cout << "number: " << number << endl;
+      total_num = total_num + number;
+      cout << "Total number: " << total_num << endl;
+    } // curly braces are optional since
+    // there is only one statement
+    mean =
+        static_cast<float>(total_num) / value; // note the use of the typecast
+    // operator
+    cout << "The mean average of the first " << value
+         << " positive integers is " << mean << endl;
+  } else
+    cout << "Invalid input - integer must be positive" << endl;
+
+  for (int row = 1; row <= 3; row++)
+    for (int col = 1; col <= 3; col++)
+      cout << row * col << endl;
   return 0;
 }
