ae6a4d2dd8d434e061927ded72921b98a6eb6f4b
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-03-04 22:27:33 -0800

    03-loops-files/Unit3_Assignment_Dan/for.cpp: finish for.cpp code

--- 03-loops-files/Unit3_Assignment_Dan/for.cpp
+++ 03-loops-files/Unit3_Assignment_Dan/for.cpp
@@ -3,30 +3,48 @@
 
 // Dan Dan
 
+// Exercise 1: The removal of static_cast<float> may truncates the decimal part
+// of the results. When I enter odd numbers, the result should contain non-zero
+// decimal parts. For example, if I enter the number 10, it should return 5.5
+// but without the static_cast<float> it will return 5 which is truncated and
+// wrong. When I enter even numbers, the result appear to be correct, but the
+// result should contain zero in the decimal place, but it doesn't due to the
+// truncation. For example, if I enter 11, the result should be 6.0 but it shows
+// 6 due to the truncation.
+//
+// Exercise 2: If I enter a float such as 2.99, the variable value will truncate
+// any float number since the variable value is integer. so the variable value
+// will treat the input value 2.99 as 2. In the for loop, the value number will
+// go from 1 to 2 and then stops.
+//
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int value;    // value is some positive number n
-  int total = 0;  // total holds the sum of the first n positive numbers 
-  int number;   // the amount of numbers
-  float mean;   // the average of the first n positive numbers
+int main() {
+  int n; // value is some positive number n
+  int m;
+  int total = 0; // total holds the sum of the first n positive numbers
+  int number;
+  float mean; // the average of the first n positive numbers
+  int count = 0;
 
-  cout << "Please enter a positive integer" << endl;
-  cin >> value;
+  cout << "Please enter a positive integer (start)" << endl;
+  cin >> n;
 
-  if (value > 0)
-  {
-    for (number = 1; number <= value; number++)
-    {
+  cout << "Please enter a positive integer (end)" << endl;
+  cin >> m;
+
+  if (n > 0 && m > 0) {
+    for (number = n; number <= m; number++) {
       total = total + number;
+      count++;
     } // curly braces are optional since there is only one statement
 
-    mean = static_cast<float>(total) / value; // note the use of the typecast
-                          // operator here 
-    cout << "The mean average of the first " << value
-       << " positive integers is " << mean << endl;
+    mean = static_cast<float>(total) / count; // note the use of the typecast
+                                              // operator here
+    cout << "The average for the consecutive positive integers from " << n
+         << " to " << m << " is " << mean << endl;
   }
 
   else
