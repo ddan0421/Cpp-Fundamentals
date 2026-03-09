204daf954805be429dc0088329d00a30581cd9b9
Dan Dan <ddanzk28@gmail.com>
2026-03-08 22:09:48 -0700

    04-functions/Unit4_Assignment_Dan/lab_6_1/proverb.cpp: add contents to proverb.cpp

--- 04-functions/Unit4_Assignment_Dan/lab_6_1/proverb.cpp
+++ 04-functions/Unit4_Assignment_Dan/lab_6_1/proverb.cpp
@@ -1,19 +1,20 @@
 // This program prints the proverb
 // "Now is the time for all good men to come to the aid of their party"
-// in a function (procedure) called writeProverb that is called by the main function
+// in a function (procedure) called writeProverb that is called by the main
+// function
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
 using namespace std;
 
-void writeProverb();	// This is the prototype for the writeProverb function 
+void writeProverb(); // This is the prototype for the writeProverb function
 
-int main()
-{
-	// Fill in the code to call the writeProverb function
+int main() {
+  // Fill in the code to call the writeProverb function
+  writeProverb();
 
-	return 0;
+  return 0;
 }
 
 //*********************************************************************
@@ -28,3 +29,7 @@ int main()
 // Fill in the function heading and the body of the function that will print
 // to the screen the proverb listed in the comments at the beginning of the
 // program
+void writeProverb() {
+  cout << "Now is the time for all good men to come to the aid of their party"
+       << endl;
+}
