9a02e862f7da4272af778d2c5e1bf700aa384f2d
Dan Dan <ddanzk28@gmail.com>
2026-03-09 20:27:26 -0700

    04-functions/Unit4_Assignment_Dan/lab_6_1/student_generated_code.cpp: add student genereated code

--- 04-functions/Unit4_Assignment_Dan/lab_6_1/student_generated_code.cpp
+++ 04-functions/Unit4_Assignment_Dan/lab_6_1/student_generated_code.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 
 // Dan Dan
@@ -5,4 +6,30 @@
 
 using namespace std;
 
-int main() { return 0; }
+void calculateSpeed(int, int, float &);
+
+int main() {
+  int milesTraveled, hoursTraveled;
+  float speedTraveled;
+
+  cout << fixed << setprecision(2) << showpoint;
+
+  cout << "Please input the miles traveled" << endl;
+  cin >> milesTraveled;
+  cout << "Please input the hours traveled" << endl;
+  cin >> hoursTraveled;
+
+  calculateSpeed(milesTraveled, hoursTraveled, speedTraveled);
+
+  cout << "Your speed is " << speedTraveled << " miles per hour" << endl;
+
+  return 0;
+}
+
+void calculateSpeed(int miles, int hours, float &speed) {
+  if (hours == 0) {
+    speed = 0;
+  } else {
+    speed = static_cast<float>(miles) / (hours);
+  }
+}
