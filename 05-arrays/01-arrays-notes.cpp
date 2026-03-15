7de9be85a81411430e153afdb0a6ff15a5507d37
Dan Dan <ddanzk28@gmail.com>
2026-03-14 18:24:55 -0700

    05-arrays/01-arrays-notes.cpp: add working progress note

---
+++ 05-arrays/01-arrays-notes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int tests[5];
+
+  /*
+   * int is the data type of the array elements
+   * tests is the name of the array
+   * 5, in [5], is the size declarator. It shows the number of elements in the
+   * array. The size of an aray is (number of elements) * (size of each element)
+   */
+
+  return 0;
+}
