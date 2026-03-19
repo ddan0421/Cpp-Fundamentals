a94a9b2c81936d7d1926b3c84f7c6b45b875d717
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-03-19 16:32:13 -0700

    05-arrays/Unit5_Assignment_Dan/student_generated_code.cpp: add student_generated_code.cpp

--- 05-arrays/Unit5_Assignment_Dan/student_generated_code.cpp
+++ 05-arrays/Unit5_Assignment_Dan/student_generated_code.cpp
@@ -13,4 +13,39 @@ using namespace std;
 // elements in the array and the letter category (A, B, C, D or F). The program
 // will print the number of grades that are A, B, etc.
 
-int main() {}
+const int MAXGRADES = 50;
+typedef char GradeType[MAXGRADES];
+int getCount(const GradeType &, char);
+
+int main() {
+  int numIteration;
+  GradeType letterArr;
+
+  cout << "Please input the number of grades to be read in. (1-50)" << endl;
+  cin >> numIteration;
+
+  cout << "All grades must be upper case A B C D or F" << endl;
+
+  for (int i = 0; i < numIteration; i++) {
+    cout << "Input a grade" << endl;
+    cin >> letterArr[i];
+  }
+  cout << endl << endl;
+  cout << "Number of A=" << getCount(letterArr, 'A') << endl;
+  cout << "Number of B=" << getCount(letterArr, 'B') << endl;
+  cout << "Number of C=" << getCount(letterArr, 'C') << endl;
+  cout << "Number of D=" << getCount(letterArr, 'D') << endl;
+  cout << "Number of F=" << getCount(letterArr, 'F') << endl;
+
+  return 0;
+}
+
+int getCount(const GradeType &array, char letter) {
+  int count = 0;
+  for (const auto &var : array) {
+    if (var == letter) {
+      count++;
+    }
+  }
+  return count;
+}
