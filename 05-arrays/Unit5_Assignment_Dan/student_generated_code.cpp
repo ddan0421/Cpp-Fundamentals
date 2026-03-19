fa7e265b9f8218e6920f5063933abd39a7c3a762
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-03-19 13:11:43 -0700

    05-arrays/Unit5_Assignment_Dan/student_generated_code: add student generated code for Unit 5

---
+++ 05-arrays/Unit5_Assignment_Dan/student_generated_code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// Dan Dan
+// Option 3
+
+// Write a program that will input letter grades (A, B, C, D, F), the
+// number of which is input by the user (a maximum of 50 grades). The
+// grades will be read into an array. A function will be called five times (once
+// for each letter grade) and will return the total number of grades in that
+// category. The input to the function will include the array, number of
+// elements in the array and the letter category (A, B, C, D or F). The program
+// will print the number of grades that are A, B, etc.
+
+int main() {}
