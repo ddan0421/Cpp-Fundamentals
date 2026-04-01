46aa3b06a5dd57531b4b6f93ff49da5595fc97e3
Dan Dan <ddanzk28@gmail.com>
2026-03-31 21:57:54 -0700

    06-arrays-sort-search/Unit6_Assignment_Dan/bubble_sort.cpp: add bubble_sort.cpp

--- 06-arrays-sort-search/Unit6_Assignment_Dan/bubble_sort.cpp
+++ 06-arrays-sort-search/Unit6_Assignment_Dan/bubble_sort.cpp
@@ -1,7 +1,7 @@
 // This program uses a bubble sort to arrange an array of integers in
 // ascending order
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
 using namespace std;
@@ -12,19 +12,22 @@ void displayArray(int[], int);
 
 const int SIZE = 5;
 
-int main()
-{
-	int values[SIZE] = { 9, 2, 0, 11, 5 };
+int main() {
+  int values[SIZE] = {9, 2, 0, 11, 5};
 
-	cout << "The values before the bubble sort is performed are:" << endl;
-	displayArray(values, SIZE);
+  cout << "The values before the bubble sort is performed are:" << endl;
+  displayArray(values, SIZE);
 
-	bubbleSortArray(values, SIZE);
+  cout << endl;
 
-	cout << "The values after the bubble sort is performed are:" << endl;
-	displayArray(values, SIZE);
+  bubbleSortArray(values, SIZE);
 
-	return 0;
+  cout << "The values after the bubble sort is performed are:" << endl;
+  displayArray(values, SIZE);
+
+  cout << endl;
+
+  return 0;
 }
 
 //******************************************************************
@@ -36,50 +39,50 @@ int main()
 //
 //******************************************************************
 
-void displayArray(int array[], int elems)	// function heading
-{		
-	// displays the array 
-	for (int count = 0; count < elems; count++)
-		cout << array[count] << "	" << endl;
+void displayArray(int array[], int elems) // function heading
+{
+  // displays the array
+  for (int count = 0; count < elems; count++)
+    cout << array[count] << "	";
 }
 
 //******************************************************************
 //	bubbleSortArray
 //
-//  task:	  to sort values of an array in ascending order
+//  task:	  to sort values of an array in descending order
 //  data in:  the array, the array size
 //  data out: the sorted array
 //
 //******************************************************************
 
-void bubbleSortArray(int array[], int elems)
-{
-	bool swap;
-	int temp;
-	int bottom = elems - 1;	// bottom indicates the end part of the
-							// array where the largest values have
-							// settled in order 
-	do
-	{
-		swap = false;
+void bubbleSortArray(int array[], int elems) {
+  bool swap;
+  int temp;
+  int bottom = elems - 1; // bottom indicates the end part of the
+                          // array where the largest values have
+                          // settled in order
+  do {
+    swap = false;
 
-		for (int count = 0; count < bottom; count++)
-		{
-			if (array[count] > array[count + 1])
-			{		
-				// the next three lines do a swap 
-				temp = array[count];
-				array[count] = array[count + 1];
-				array[count + 1] = temp;
+    for (int count = 0; count < bottom; count++) {
+      if (array[count] < array[count + 1]) {
+        // the next three lines do a swap
+        temp = array[count];
+        array[count] = array[count + 1];
+        array[count + 1] = temp;
 
-				swap = true; // indicates that a swap occurred
-			}
-		}
+        swap = true; // indicates that a swap occurred
 
-		bottom--;	// bottom is decremented by 1 since each pass through
-					// the array adds one more value that is set in order
+        // display array at each step
+        displayArray(array, SIZE);
+        cout << endl;
+      }
+    }
 
-	} while (swap != false);
-	// loop repeats until a pass through the array with
-	// no swaps occurs
+    bottom--; // bottom is decremented by 1 since each pass through
+              // the array adds one more value that is set in order
+
+  } while (swap != false);
+  // loop repeats until a pass through the array with
+  // no swaps occurs
 }
