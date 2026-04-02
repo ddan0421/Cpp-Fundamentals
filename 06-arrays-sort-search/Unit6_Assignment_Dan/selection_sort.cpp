b11b3f78422cf1a70914d2441c355b75ec1773e9
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-04-01 17:58:48 -0700

    06-arrays-sort-search/Unit6_Assignment_Dan/selection_sort.cpp: add selection_sort contents

--- 06-arrays-sort-search/Unit6_Assignment_Dan/selection_sort.cpp
+++ 06-arrays-sort-search/Unit6_Assignment_Dan/selection_sort.cpp
@@ -1,7 +1,7 @@
 // This program uses a selection sort to arrange an array of integers in
 // ascending order
 
-// PLACE YOUR NAME HERE
+// Dan Dan
 
 #include <iostream>
 using namespace std;
@@ -10,83 +10,86 @@ using namespace std;
 void selectionSortArray(int[], int);
 void displayArray(int[], int);
 
-const int SIZE = 5;
+const int SIZE = 8;
 
-int main()
-{
-	int values[SIZE] = { 9, 2, 0, 11, 5 };
+int main() {
+  int values[SIZE] = {23, 0, 45, -3, -78, 1, -1, 9};
 
-	cout << "The values before the selection sort is performed are:" << endl;
-	displayArray(values, SIZE);
+  cout << "The values before the selection sort is performed are:" << endl;
+  displayArray(values, SIZE);
 
-	selectionSortArray(values, SIZE);
+  cout << endl;
 
-	cout << "The values after the selection sort is performed are:" << endl;
-	displayArray(values, SIZE);
+  selectionSortArray(values, SIZE);
 
-	return 0;
+  cout << "The values after the selection sort is performed are:" << endl;
+  displayArray(values, SIZE);
+
+  cout << endl;
+
+  return 0;
 }
 
 //******************************************************************
-//	displayArray
+//  displayArray
 //
-//  task:	  to print the array
+//  task:   to print the array
 //  data in:  the array to be printed, the array size
 //  data out: none
 //
 //******************************************************************
 
-void displayArray(int array[], int elems)	// function heading
-{		
-	// Displays array 
-	for (int count = 0; count < elems; count++)
-		cout << array[count] << " ";
-	cout << endl;
+void displayArray(int array[], int elems) // function heading
+{
+  // Displays array
+  for (int count = 0; count < elems; count++)
+    cout << array[count] << " ";
+  cout << endl;
 }
 
 //******************************************************************
-//	selectionSortArray
+//  selectionSortArray
 //
-//  task:	  to sort values of an array in ascending order
+//  task:   to sort values of an array in descending order
 //  data in:  the array, the array size
 //  data out: the sorted array
 //
 //******************************************************************
 
-void selectionSortArray(int array[], int elems)
-{
-	int seek;		// array position currently being put in order 
-	int minCount;	// location of smallest value found
-	int minValue;	// holds the smallest value found
+void selectionSortArray(int array[], int elems) {
+  int seek;     // array position currently being put in order
+  int maxCount; // location of smallest value found
+  int maxValue; // holds the smallest value found
 
-	for (seek = 0; seek < (elems - 1); seek++)	// outer loop performs the swap
-												// and then increments seek
-	{
-		minCount = seek;
-		minValue = array[seek];
+  for (seek = 0; seek < (elems - 1); seek++) // outer loop performs the swap
+                                             // and then increments seek
+  {
+    maxCount = seek;
+    maxValue = array[seek];
 
-		for (int index = seek + 1; index < elems; index++)
-		{
-			// inner loop searches through array
-			// starting at array[seek] searching
-			// for the smallest value. When the
-			// value is found, the subscript is
-			// stored in minCount. The value is
-			// stored in minValue.
+    for (int index = seek + 1; index < elems; index++) {
+      // inner loop searches through array
+      // starting at array[seek] searching
+      // for the smallest value. When the
+      // value is found, the subscript is
+      // stored in minCount. The value is
+      // stored in minValue.
 
-			if (array[index] < minValue)
-			{
-				minValue = array[index];
-				minCount = index;
-			}
-		}
+      if (array[index] > maxValue) {
+        maxValue = array[index];
+        maxCount = index;
+      }
+    }
 
-		// the following two statements exchange the value of the
-		// element currently needing the smallest value found in the
-		// pass(indicated by seek) with the smallest value found
-		// (located in minValue)
+    // the following two statements exchange the value of the
+    // element currently needing the smallest value found in the
+    // pass(indicated by seek) with the smallest value found
+    // (located in minValue)
 
-		array[minCount] = array[seek];
-		array[seek] = minValue;
-	}
+    array[maxCount] = array[seek];
+    array[seek] = maxValue;
+
+    displayArray(array, SIZE);
+    cout << endl;
+  }
 }
