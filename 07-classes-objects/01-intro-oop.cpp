1648dd7920724de147827045e17c1f97bbe3a067
Dan Dan <ddanzk28@gmail.com>
2026-04-07 22:11:01 -0700

    07-classes-objects/01-intro-oop.cpp: add initial notes

---
+++ 07-classes-objects/01-intro-oop.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+// Procedural programming focuses on the process/actions that occur in a
+// program
+//
+// Object-Oriented programming is based on the data and the functions
+// that operate on it.  Objects are instances of ADTs that represent the data
+// and its functions
+//
+// class is an abstract data type (ADT) which is a user defined data type that
+// combines a collection of variables and operations
+//
+//
+// A class consists of a name (its identity), its member data which describes
+// what it is and its member functions which describe what it does.
+//
+// class: like a struct (allows bundling of related variables),  but variables
+// and functions in the class can have different properties than in a struct
+//
+// object: an instance of a class, in the same way that a variable can be an
+// instance of a struct
+//
+// In object-oriented programming (OOP), a **class** is a blueprint or template
+// used to define the structure and behavior of a type of object. It specifies
+// what data the object will store (attributes) and what actions it can perform
+// (methods). In C++, a class is declared using the `class` keyword and
+// typically contains variables and functions grouped together to model a
+// real-world concept.
+//
+// An **object** is a concrete instance created from a
+// class. While a class defines the general idea, an object represents an actual
+// usable entity with specific values. When you create an object in C++, memory
+// is allocated for it, and it can then be used to access the attributes and
+// methods defined in its class.
+//
+// An **instance** is simply another term for an
+// object. Saying that an object is an instance of a class emphasizes that it
+// was created from that class. In practice, “object” and “instance” are used
+// interchangeably in most programming contexts.
+//
+// An **attribute** (also called a data member in C++) is a variable defined
+// inside a class that represents the state or properties of an object. Each
+// object has its own copy of these attributes, allowing different objects of
+// the same class to store different data.
+//
+// A **method** (also called a member function) is a function defined
+// inside a class that describes the behaviors or actions an object can perform.
+// Methods can access and modify the object’s attributes, allowing the object to
+// operate on its own data.
+//
+//
+//
+// Data and functions that are defined as public can be directly accessed by
+// code outside the class, while functions and data defined as private can be
+// accessed only by functions belonging to the class. Usually, classes make data
+// members private and require outside access to them through member functions
+// that are defined as public. Member functions are thus usually defined as
+// public and member data as private.used
+
+// Class declaration (header file)
+class Rectangle // Rectangle is the name of the class (its identity).
+{
+public:
+  // The following are labeled as public.
+  // Usually member functions are defined public
+  // and are used to describe what the class can do.
+  void setLength(float side_l);
+  // This member function receives the length of the
+  // Rectangle object that calls it and places that value in
+  // the member data called length.
+  void setWidth(float side_w);
+  // This member function receives the width of the Rectangle
+  // object that calls it and places the value in the member
+  // data called width.
+  float getLength();
+  // This member function returns the length of the Rectangle
+  // object that calls it.
+  float getWidth();
+  // This member function returns the width of the Rectangle
+  // object that calls it.
+  double findArea();
+  // This member function finds the area of the Rectangle object
+  // that calls it.
+  double findPerimeter();
+  // This member function finds the perimeter of the Rectangle
+  // object that calls it.
+
+private:
+  // The following are labeled as private.
+  // Member data are usually declared private so they can
+  // ONLY be accessed by functions that belong to the class.
+  // Member data describe the attributes of the class
+  float length;
+  float width;
+};
