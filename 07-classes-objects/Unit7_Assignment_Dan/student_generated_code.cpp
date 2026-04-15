301bcc45fcd9c55c299b5229695fc97fc1ff3250
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-04-14 17:15:57 -0700

    07-classes-objects/Unit7_Assignment_Dan/student_generated_code.cpp: add some progress but still not finished

--- 07-classes-objects/Unit7_Assignment_Dan/student_generated_code.cpp
+++ 07-classes-objects/Unit7_Assignment_Dan/student_generated_code.cpp
@@ -6,8 +6,21 @@ class SavingsAccount {
 public:
   SavingsAccount();
   ~SavingsAccount();
+  void initialDollars(float initDollars);
+  void initialCents(float initCents);
+
+  void depositDollars(float inputDollars);
+  void depositCents(float inputCents);
+  void withdrawDollars(float outDollars);
+  void withdrawCents(float outCents);
+
+  void displayTotal();
 
 private:
   float dollars;
   float cents;
+};
+
+void SavingsAccount::initialDollars(float initDollars) {
+  dollars = initDollars;
 }
