7ec0dd59b3bf197b2566e9776a10afcea5e5b6a5
Dan Dan <ddanzk28@gmail.com>
2026-04-20 21:42:43 -0700

    08-more-classes-objects/01-more-oop.ccp: add more notes

--- 08-more-classes-objects/01-more-oop.cpp
+++ 08-more-classes-objects/01-more-oop.cpp
@@ -33,6 +33,23 @@ int Car::count = 0;
 // Definition and initialization outside the class
 int Tree::objectCount = 0;
 
+int main() {
+  Tree oak;
+  Tree elm;
+  Tree pine;
+  cout << "We have " << pine.getObjectCount() << " trees in our program!"
+       << endl;
+
+  // static member function example
+  cout << Car::getCount() << endl; // called without object
+
+  Car a, b;
+  Car::count = 2;
+
+  cout << Car::getCount() << endl; // 2
+  return 0;
+}
+
 // Friend: A friend function is not a member of the class but has access to its
 // private members.
 
@@ -79,19 +96,137 @@ public:
 //     return 0;
 // }
 
+// Copy Constructors
+// Special constructor used when a newly created object is initialized to the
+// data of another object of same class Default copy constructor copies
+// field-to-field Default copy constructor works fine in many cases
+
+/*
+class Box {
+public:
+    int value;
+
+    Box(int v) {
+        value = v;
+    }
+
+    // Copy constructor
+    Box(const Box& other) { //the parameter is another Box object
+        value = other.value; //Copy the value from the old object into the new
+one. This line value = other.value; is a manual copy. For pointers, not enough
+(needs deep copy)
+    }
+};
+
+
 int main() {
-  Tree oak;
-  Tree elm;
-  Tree pine;
-  cout << "We have " << pine.getObjectCount() << " trees in our program!"
-       << endl;
+    Box b1(10);
+    Box b2 = b1; // copy constructor is called. Whenever this happens, C++
+automatically looks for a copy constructor
 
-  // static member function example
-  cout << Car::getCount() << endl; // called without object
-
-  Car a, b;
-  Car::count = 2;
-
-  cout << Car::getCount() << endl; // 2
-  return 0;
+    cout << b2.value << endl; // 10
 }
+*/
+
+/* however, with pointer is a little complicated:
+class Box {
+public:
+    int* value; // pointer to an int. the variable value doest NOT store a
+number but the address of a number in memory
+
+    Box(int v) {
+        value = new int(v); //input an actual value v and the variable "value"
+is the pointer (address) to a new copy of v, not v itself
+    }
+
+      // SHALLOW COPY (default-style)
+    Box(const Box& other) {
+        value = other.value; // ❌ copies address only
+    }
+
+    ~Box() {
+        delete value;
+    }
+};
+
+Now copy it
+Box b1(10);
+Box b2 = b1; // copy constructor. This copies the pointer address, NOT the data
+             // b2.value = b1.value.
+             // Both objects point to the same memory
+             // Modifying one affects the other
+             // If you add a destructor then it will destruct both b1 and b2
+
+so: value = other.value; // ❌ shallow copy
+
+The fix:
+
+class Box {
+public:
+    int* value;
+
+    Box(int v) {
+        value = new int(v);
+    }
+
+    // DEEP COPY
+    Box(const Box& other) {
+        value = new int(*other.value); // ✅ copy actual value
+    }
+
+    ~Box() {
+        delete value;
+    }
+};
+
+*/
+
+// Operator Overloading
+/*
+class Complex {
+public:
+    int real, imag;
+
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        imag = i;
+    }
+
+    // operator overloading for +
+    Complex operator+(const Complex& other) {
+        return Complex(real + other.real, imag + other.imag);
+    }
+};
+
+
+Complex c1(2, 3);
+Complex c2(1, 4);
+
+Complex c3 = c1 + c2;
+
+
+This happends internally: c1.operator+(c2);
+
+*/
+
+// The this Pointer
+// when you call obj.show(); Inside show(), C++ automatically poasses a hidden
+// pointer: this -> points to obj
+// So the function can konw whic object is being used.
+/*
+class Test {
+private:
+    int x;
+
+public:
+    Test(int x) {
+        this->x = x;  // disambiguation
+    }
+
+    void show() {
+        cout << this->x << endl;
+    }
+};
+*/
+// this -> x is the object's member variable
+// x is the parameter
