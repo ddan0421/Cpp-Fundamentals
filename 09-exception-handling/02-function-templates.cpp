a5fb775d524dd6f9d2433a78057609cbfd37b8d5
Dan Dan <ddan@pnmac.com>
2026-04-27 21:47:48 -0700

    09-exception-handling/02-function-templates.cpp: add more notes

--- 09-exception-handling/02-function-templates.cpp
+++ 09-exception-handling/02-function-templates.cpp
@@ -47,4 +47,61 @@ int main(){
 
 
 
+/*
+Function templates can be overloaded Each template must have a unique parameter list
+	template <typename T>
+	T sumAll(T num) ...
+
+	template <typename T1, typename T2>
+	T1 sumall(T1 num1, T2 num2) ...
+
+
+All data types specified in template prefix must be used in template definition
+Function calls must pass parameters for all data types specified in the template prefix
+Like regular functions, function templates must be defined before being called
+
+
+A function template is a pattern
+No actual code is generated until the function named in the template is called
+A function template uses no memory 
+When passing a class object to a function template, ensure that all operators in the template are defined or overloaded in the class definition
+
+*/
+
+
+/*
+Where to start when defining templates
+Templates are often appropriate for multiple functions that perform the same task with different parameter data types
+Develop function using usual data types first, then convert to a template:
+add template prefix
+convert data type names in the function to a type parameter (i.e., a T type) in the template
+
+*/
+
+
+// Class Templates
+/*
+Classes can also be represented by templates.  When a class object is created, type information is supplied to define the type of data members of the class.
+Unlike functions, classes are instantiated by supplying the type name (int, double, string, etc.) at object definition
+
+
+template <typename T>
+class grade
+{	
+	  private:
+			T score;
+		 public:
+			grade(T);
+			void setGrade(T);
+			T getGrade()
+};
+
+*/
+
+
+
+
+
+
+
 
