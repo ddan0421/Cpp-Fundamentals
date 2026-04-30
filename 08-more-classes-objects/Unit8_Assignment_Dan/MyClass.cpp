2eb5eaf2a988e9c60e5bbe801752b1592e4c4351
Dan Dan <ddan@pnmac.com>
2026-04-29 18:03:55 -0700

    08-more-classes-objects/Unit8_Assignment_Dan/MyClass.cpp: add a getter to print out the value

--- 08-more-classes-objects/Unit8_Assignment_Dan/MyClass.cpp
+++ 08-more-classes-objects/Unit8_Assignment_Dan/MyClass.cpp
@@ -8,16 +8,18 @@ private:
 
 public:
   MyClass(int v) : value(v) {}
+  void getValue() {cout << "The value is " << value << endl;}
   friend void showValue(MyClass &obj);
 };
 
 void showValue(MyClass &obj) {
   obj.value = 2; // update the private variable
-  cout << "Value: " << obj.value << endl;
+  cout << "After updating, the value is now: " << obj.value << endl;
 }
 
 int main() {
   MyClass class1(1);
+  class1.getValue();
   showValue(class1);
 
   return 0;
