492d874c0dd73100f73dd1b285a3eaabf1c00a01
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-04-30 16:44:44 -0700

    10-pointers/02-arrays-and-pointers.cpp: add arrays and pointers notes (still working)

---
+++ 10-pointers/02-arrays-and-pointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+When arrays are passed to functions they are passed by pointer. An array name
+is a pointer to the beginning of the array. Variables can hold just one value
+and so we can reference that value by just naming the variable. Arrays, however,
+hold many values. All of these values cannot be referenced just by naming the
+array.
+
+
+This is where pointers enter the picture. Pointers allow us to access all the
+array elements.
+
+!array name is really a pointer that holds the address of the first element in
+the array.!
+
+
+
+
+*/
+
+int main() { return 0; }
