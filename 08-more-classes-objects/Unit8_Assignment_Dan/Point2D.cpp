9f05b2acee8e5ec4448d12c5c5e69758d8be8ac5
Dan Dan <ddan@pnmac.com>
2026-04-29 18:17:26 -0700

    08-more-classes-objects/Unit8_Assignment_Dan/Point2D.cpp: modify the operator+ overload to simply adding two points

--- 08-more-classes-objects/Unit8_Assignment_Dan/Point2D.cpp
+++ 08-more-classes-objects/Unit8_Assignment_Dan/Point2D.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -11,16 +12,22 @@ public:
     double operator-(const Point2D& right) {
         return sqrt(pow((x - right.x), 2) + pow((y - right.y), 2));
     }
-    double operator+(const Point2D& right) {
-        return sqrt(pow((x + right.x), 2) + pow((y + right.y), 2));
+    Point2D operator+(const Point2D& right) {
+        return Point2D(x + right.x,  y + right.y);
     }    
+
+    string printPoint2D(){
+        return "point is (" + to_string(x) + ", " + to_string(y) + ")"; 
+    }
 };
 
 int main() {
     Point2D point1(2,2);
     Point2D point2(3,4);
 
+    Point2D addition = point1 + point2;
+
     cout << "The subtraction result is " << point1 - point2 << endl;
-    cout << "The addition result is " << point1 + point2 << endl;
+    cout << "The addition result is " << addition.printPoint2D() << endl;
     return 0;
 }
