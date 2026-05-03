e735ca4e161a4d527b1213b6e7aebe0d5b84a0de
Dan Dan <ddanzk28@gmail.com>
2026-05-02 18:07:40 -0700

    10-pointers/Unit10_Assignment_Dan/dynamic.cpp: add dynamic.cpp contents

--- 10-pointers/Unit10_Assignment_Dan/dynamic.cpp
+++ 10-pointers/Unit10_Assignment_Dan/dynamic.cpp
@@ -1,59 +1,79 @@
 // This program demonstrates the use of dynamic variables
 
-// PLACE YOUR NAME HERE
+// Dan Dan
+
+/*
+Exercise 2: A bracketed subscript is unnecessary because when you write
+name[pos], the compiler literally interprets it as *(name + pos);
+
+name[pos] works for inputting the name because name[pos] tells the compiler
+where in memory to store the character that user types in
+
+name[pos] worsk for outputting the name because name[pos] returns the value of
+the character that stored at that address.
+
+*/
 
 #include <iostream>
 using namespace std;
 
 const int MAXNAME = 10;
 
-int main()
-{
-	int pos;
-	char *name = nullptr;
-	int *one = nullptr;
-	int *two = nullptr;
-	int *three = nullptr;
-	int result;
+int main() {
+  int pos;
+  char *name = nullptr;
+  int *one = nullptr;
+  int *two = nullptr;
+  int *three = nullptr;
+  int result;
 
-	//	Fill in code to allocate the integer variable one here
+  //	Fill in code to allocate the integer variable one here
+  one = new int;
 
-	//	Fill in code to allocate the integer variable two here
+  //	Fill in code to allocate the integer variable two here
+  two = new int;
 
-	//	Fill in code to allocate the integer variable three here
+  //	Fill in code to allocate the integer variable three here
+  three = new int;
 
-	//	Fill in code to allocate the character array pointed to by name
+  //	Fill in code to allocate the character array pointed to by name
+  name = new char[MAXNAME];
+  cout << "Enter your last name with exactly 10 characters." << endl;
+  cout << "If your name has < 10 characters, repeat last letter. " << endl
+       << "Blanks at the end do not count." << endl;
 
-	cout << "Enter your last name with exactly 10 characters." << endl;
-	cout << "If your name has < 10 characters, repeat last letter. " << endl
-		 << "Blanks at the end do not count." << endl;
+  for (pos = 0; pos < MAXNAME; pos++)
+    cin >> *(name + pos); // Fill in code to read a character into the name
+                          // array WITHOUT USING a bracketed subscript
 
-	for (pos = 0; pos < MAXNAME; pos++)
-		cin >>	// Fill in code to read a character into the name array
-				// WITHOUT USING a bracketed subscript
+  cout << "Hi ";
+  for (pos = 0; pos < MAXNAME; pos++)
+    cout << *(name + pos); // Fill in code to a print a character from the name
+                           // array WITHOUT USING a bracketed subscript
 
-	cout << "Hi ";
+  cout << endl << "Enter three integer numbers separated by blanks" << endl;
 
-	for (pos = 0; pos < MAXNAME; pos++)
-		cout << // Fill in code to a print a character from the name array
-				// WITHOUT USING a bracketed subscript
+  // Fill in code to input three numbers and store them in the
+  // dynamic variables pointed to by pointers one, two, and three.
+  // You are working only with pointer variables
+  cin >> *one >> *two >> *three;
 
-	cout << endl << "Enter three integer numbers separated by blanks" << endl;
+  // echo print
+  cout << "The three numbers are ";
 
-	// Fill in code to input three numbers and store them in the
-	// dynamic variables pointed to by pointers one, two, and three.
-	// You are working only with pointer variables
+  // Fill in code to output those numbers
+  cout << *one << " " << *two << " " << *three << endl;
 
-	// echo print
-	cout << "The three numbers are " << endl;
+  result = *one + *two +
+           *three; // Fill in code to calculate the sum of the three numbers
 
-	// Fill in code to output those numbers
+  cout << "The sum of the three values is " << result << endl;
 
-	result = // Fill in code to calculate the sum of the three numbers
+  // Fill in code to deallocate one, two, three and name
+  delete one;
+  delete two;
+  delete three;
+  delete[] name;
 
-	cout << "The sum of the three values is " << result << endl;
-
-	// Fill in code to deallocate one, two, three and name
-
-	return 0;
+  return 0;
 }
