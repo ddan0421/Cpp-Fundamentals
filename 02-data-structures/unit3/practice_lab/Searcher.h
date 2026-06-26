aaa581bf343dc259635e088ce323d821a6cfb0b9
Dan Dan <ddanzk28@gmail.com>
2026-06-25 21:10:36 -0700

    02-data-structures/unit3/practice_lab/Searcher.h: add binarySearch contents

--- 02-data-structures/unit3/practice_lab/Searcher.h
+++ 02-data-structures/unit3/practice_lab/Searcher.h
@@ -3,16 +3,27 @@
 
 #include "Comparer.h"
 
-template <typename T>
-class Searcher {
+template <typename T> class Searcher {
 public:
-   // Returns the index of the key in the sorted array, or -1 if the key is not
-   // found
-   static int BinarySearch(T* array, int arraySize, const T& key,
-      Comparer<T>& comparer) {
-      // TODO: Type your code here
-      return -1;
-   }
+  // Returns the index of the key in the sorted array, or -1 if the key is not
+  // found
+  static int BinarySearch(T *array, int arraySize, const T &key,
+                          Comparer<T> &comparer) {
+    // TODO: Type your code here
+    int low = 0;
+    int high = arraySize - 1;
+    while (low <= high) {
+      int middle = low + (high - low) / 2;
+      if (comparer.Compare(array[middle], key) == 0) {
+        return middle;
+      } else if (comparer.Compare(array[middle], key) < 0) {
+        low = middle + 1;
+      } else {
+        high = middle - 1;
+      }
+    }
+    return -1;
+  }
 };
 
 #endif
