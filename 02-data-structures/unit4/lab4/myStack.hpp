9312fbc069679d90f58ff6652ea2042810b6e89a
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-07-06 14:47:59 -0700

    02-data-structures/unit4/lab4/myStack.hpp: delete 'TODO' comments

--- 02-data-structures/unit4/lab4/myStack.hpp
+++ 02-data-structures/unit4/lab4/myStack.hpp
@@ -48,9 +48,6 @@ public:
    * Default constructor
    * POST:
    *    The stack is empty.
-   *
-   * TODO:
-   * - Initialize this stack so it starts empty.
    ******************************************************************/
   myStack();
 
@@ -61,9 +58,6 @@ public:
    * POST:
    *    This stack becomes a deep copy of otherStack.
    *    Modifying one stack must NOT affect the other.
-   *
-   * TODO:
-   * - Reuse base-class copy behavior.
    ******************************************************************/
   myStack(const myStack<type> &otherStack);
 
@@ -71,9 +65,6 @@ public:
    * Destructor
    * POST:
    *    All dynamically allocated memory used by this stack is released.
-   *
-   * TODO:
-   * - If the base class already handles cleanup, keep this simple.
    ******************************************************************/
   ~myStack();
 
@@ -85,9 +76,6 @@ public:
    *    This stack becomes a deep copy of rhs.
    *    Self-assignment is handled safely.
    *    Returns *this.
-   *
-   * TODO:
-   * - Reuse base-class assignment behavior.
    ******************************************************************/
   myStack<type> &operator=(const myStack<type> &rhs);
 
@@ -95,9 +83,6 @@ public:
    * isEmptyStack
    * POST:
    *    Returns true if the stack is empty and false otherwise.
-   *
-   * TODO:
-   * - Reuse inherited linked-list logic.
    ******************************************************************/
   bool isEmptyStack() const;
 
@@ -108,10 +93,6 @@ public:
    * POST:
    *    theItem is added to the TOP of the stack.
    *    count increases by 1.
-   *
-   * TODO:
-   * - TOP = FRONT of linked list.
-   * - Do NOT use addLast.
    ******************************************************************/
   void push(const type &theItem);
 
@@ -122,10 +103,6 @@ public:
    * POST:
    *    If the stack is not empty, ONLY the TOP element is removed.
    *    If the stack is empty, the stack remains unchanged.
-   *
-   * TODO:
-   * - TOP = FRONT of linked list.
-   * - pop does NOT return a value.
    ******************************************************************/
   void pop();
 
@@ -135,9 +112,6 @@ public:
    *    The stack is not empty.
    * POST:
    *    Returns the TOP element without modifying the stack.
-   *
-   * TODO:
-   * - Reuse the correct inherited accessor.
    ******************************************************************/
   type getTop() const;
 
@@ -145,9 +119,6 @@ public:
    * getCount
    * POST:
    *    Returns the number of elements currently in the stack.
-   *
-   * TODO:
-   * - Reuse inherited count logic.
    ******************************************************************/
   int getCount() const;
 
@@ -160,10 +131,6 @@ public:
    *       EMPTY STACK
    *    Otherwise prints the stack from TOP to BOTTOM,
    *    one value per line.
-   *
-   * TODO:
-   * - Match the required output format exactly.
-   * - Do NOT call myLinkedList::print().
    ******************************************************************/
   void print(std::ostream &outStream) const;
 };
@@ -174,9 +141,6 @@ public:
  *    outStream is a valid output stream.
  * POST:
  *    Outputs the stack using print(outStream), then returns outStream.
- *
- * TODO:
- * - Usually just call theStack.print(outStream);
  ***********************************************************************/
 template <class type>
 std::ostream &operator<<(std::ostream &outStream,
@@ -185,23 +149,19 @@ std::ostream &operator<<(std::ostream &outStream,
 // ================= STUDENT IMPLEMENTATIONS =================
 
 template <class type> myStack<type>::myStack() {
-  // TODO: Initialize this stack so it starts empty.
   // Base class default constructor initializes an empty list/stack.
 }
 
 template <class type> myStack<type>::myStack(const myStack<type> &otherStack) {
-  // TODO: Make this stack a deep copy of otherStack.
   myLinkedList<type>::operator=(otherStack);
 }
 
 template <class type> myStack<type>::~myStack() {
-  // TODO: If no extra dynamic memory is introduced here,
-  // the base class destructor may already handle cleanup.
+  // Base class destructor releases all linked-list nodes.
 }
 
 template <class type>
 myStack<type> &myStack<type>::operator=(const myStack<type> &rhs) {
-  // TODO: Handle self-assignment safely and reuse base-class logic.
   if (this != &rhs) {
     myLinkedList<type>::operator=(rhs);
   }
@@ -209,35 +169,28 @@ myStack<type> &myStack<type>::operator=(const myStack<type> &rhs) {
 }
 
 template <class type> bool myStack<type>::isEmptyStack() const {
-  // TODO: Return true if the stack is empty, false otherwise.
   return myLinkedList<type>::isEmpty();
 }
 
 template <class type> void myStack<type>::push(const type &theItem) {
-  // TODO: Add theItem to the TOP of the stack.
   myLinkedList<type>::addFirst(theItem);
 }
 
 template <class type> void myStack<type>::pop() {
-  // TODO: Remove ONLY the TOP element if the stack is not empty.
   if (!isEmptyStack()) {
     myLinkedList<type>::deleteAt(0);
   }
 }
 
 template <class type> type myStack<type>::getTop() const {
-  // TODO: Return the TOP element without modifying the stack.
   return myLinkedList<type>::getFirst();
 }
 
 template <class type> int myStack<type>::getCount() const {
-  // TODO: Return the number of elements currently in the stack.
   return myLinkedList<type>::getCount();
 }
 
 template <class type> void myStack<type>::print(std::ostream &outStream) const {
-  // TODO: Print EMPTY STACK if the stack is empty.
-  // Otherwise print each value from TOP to BOTTOM, one per line.
   if (isEmptyStack()) {
     outStream << "EMPTY STACK" << "\n";
     return;
@@ -254,7 +207,6 @@ template <class type> void myStack<type>::print(std::ostream &outStream) const {
 template <class type>
 std::ostream &operator<<(std::ostream &outStream,
                          const myStack<type> &theStack) {
-  // TODO: Call theStack.print(outStream);
   theStack.print(outStream);
   return outStream;
 }
